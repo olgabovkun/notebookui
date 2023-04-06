8f7c1a4a55bb1a5d5ad206557abb0e6e69348629
Olga Bovkun <olga.bovkun.ob@gmail.com>
2023-04-06 15:10:53 -0500

    add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { PageNotFoundComponent } from './partials/notfound/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
@@ -11,7 +12,17 @@ const routes: Routes = [
   { path: 'register', component: SignupComponent },
   { path: 'home', component: HomeComponent },
   { path: 'home/note/:id', component: HomeComponent },
-  // { path: '**', component: PageNotFoundComponent }, // todo
+  { path: '**', redirectTo: 'not-found' },
+  {
+    path: 'not-found',
+    data: { title: '404' },
+    children: [
+      {
+        path: '',
+        component: PageNotFoundComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
